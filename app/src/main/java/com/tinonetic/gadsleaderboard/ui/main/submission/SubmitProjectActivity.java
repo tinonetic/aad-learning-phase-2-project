faa4ec57cfab0297c1d05599a9b8fd99c16dbfcd
Tinomuda Mahunyepa <tinonetic@gmail.com>
2020-09-11 22:19:37 +0200

    Added activity for submmitting project and Up button. To fix Toolbar customisation

---
+++ app/src/main/java/com/tinonetic/gadsleaderboard/ui/main/submission/SubmitProjectActivity.java
@@ -0,0 +1,14 @@
+package com.tinonetic.gadsleaderboard.ui.main.submission;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubmitProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_project);
+    }
+}
